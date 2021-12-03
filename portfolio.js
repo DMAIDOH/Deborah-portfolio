7656cd63f3e2457939a54ac27f00e9d3f36a4017
DMAIDOH <deborahmaidoh@gmail.com>
2021-12-03 02:21:08 -0500

    j

---
+++ portfolio.js
@@ -0,0 +1,5 @@
+function showMenu(){
+    /*when the user clicks on the hamburger,
+    toggle between hiding and showing the menu items*/
+    document.getElementById("menu-items").classList.toggle("show");
+}
